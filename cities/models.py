f9d68e32c8a87ca2cb3e10a85923eb78273c2dff
Val <veaks@yandex.ru>
2021-07-21 23:22:36 +0300

    make model

---
+++ cities/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class City(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Город'
+        verbose_name_plural = 'Города'
+        ordering = ['name']
+
+
+
