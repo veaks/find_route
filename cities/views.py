f6d21033709247c1f70e264c36872e126c16a389
Val <veaks@yandex.ru>
2021-07-22 22:02:39 +0300

    detail_view

--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
 
-# Create your views here.
+from cities.models import City
+
+__all__ = (
+    'home', 'CityDetailView',
+)
+
+
+def home(request, pk=None):
+    #if pk:
+        # city = City.objects.filter(id=pk).first()
+        # city = City.objects.get(id=pk)
+        #city = get_object_or_404(City, id=pk)
+        #context = {'object': city}
+        #return render(request, 'cities/detail.html', context)
+    qs = City.objects.all()
+    context = {'objects_list': qs}
+    return render(request, 'cities/home.html', context)
+
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
