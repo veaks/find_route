4e952f20d4786f09aebe6dbfd7f2c52876470c45
Val <veaks@yandex.ru>
2021-07-23 22:48:13 +0300

    pagination

--- cities/views.py
+++ cities/views.py
@@ -1,25 +1,75 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 
+from cities.forms import CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView',
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView', 'CityListView'
 )
 
 
 def home(request, pk=None):
+    if request.method == 'POST':
+        form = CityForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            form.save()
     #if pk:
         # city = City.objects.filter(id=pk).first()
         # city = City.objects.get(id=pk)
         #city = get_object_or_404(City, id=pk)
         #context = {'object': city}
         #return render(request, 'cities/detail.html', context)
+    form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs}
+    lst = Paginator(qs, 3)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+    context = {'page_obj': page_obj, 'form': form}
     return render(request, 'cities/home.html', context)
 
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
+
+
+class CityCreateView(CreateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/create.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityUpdateView(UpdateView):
+    model = City
+    form_class = CityForm
+    template_name = 'cities/update.html'
+    success_url = reverse_lazy('cities:home')
+
+
+class CityDeleteView(DeleteView):
+    model = City
+    #template_name = 'cities/delete.html'
+    success_url = reverse_lazy('cities:home')
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+
+class CityListView(ListView):
+    paginate_by = 3
+    model = City
+    template_name = 'cities/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CityForm()
+        context['form'] = form
+        return context
+
+
+
