a0fe2ac4d59b534c20e1deac95d4d4f8f6cba752
Val <veaks@yandex.ru>
2021-08-01 12:59:25 +0300

    route sort

--- routes/utils.py
+++ routes/utils.py
@@ -46,5 +46,38 @@ def get_routes(request, form) -> dict:
                 right_ways.append(route)
         if not right_ways:
             raise ValueError("Машрут через эти города невозможен")
-
+    else:
+        right_ways = all_ways
+    routes = []
+    all_trains = {}
+    for q in qs:
+        all_trains.setdefault((q.from_sity_id, q.to_city_id), [])
+        all_trains[(q.from_sity_id, q.to_city_id)].append(q)
+        for route in right_ways:
+            tmp = {}
+            tmp['trains'] = []
+            total_time = 0
+            for i in range(len(route) - 1):
+                qs = all_trains[(route[i], route[i + 1])]
+                q = qs[0]
+                total_time += q.travel_time
+                tmp['trains'].append(q)
+            tmp['total_time'] = total_time
+            if total_time <= travelling_time:
+                routes.append(tmp)
+    if not routes:
+        raise ValueError('Время в пути больше заданного')
+    sorted_routes = []
+    if len(routes) == 1:
+        sorted_routes = routes
+    else:
+        times = list(set(r['total_time'] for r in routes))
+        times = sorted(times)
+        for time in times:
+            for route in routes:
+                if time == route['total_time']:
+                    sorted_routes.append(route)
+    context['routes'] = sorted_routes
+    context['cities'] = {'from_city': from_city.name, 'to_city': to_city.name}
     return context
+
